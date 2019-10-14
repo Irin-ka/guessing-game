48859243ae3d021bc2ea03169e12ffc46e73d186
ira <irin-ka@inbox.ru>
2019-10-14 21:33:18 +0300

    add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,25 @@
 class GuessingGame {
-    constructor() {}
-
-    setRange(min, max) {
-
-    }
-
-    guess() {
-
-    }
-
-    lower() {
-
-    }
-
-    greater() {
-
-    }
+    constructor(min, max) {
+        this.min = min;
+        this.max = max;
+        this.number = 0;
+     }  
+     setRange(min, max) {
+     this.min = min;
+     this.max = max;
+     }
+     guess() {
+     this.number=Math.ceil((this.max+this.min)/2);
+     console.log(this.number);
+     return this.number; 
+     }
+     lower() {
+       this.max=this.number;
+     }
+ 
+     greater() {
+      this.min=this.number;
+     }
 }
 
 module.exports = GuessingGame;
